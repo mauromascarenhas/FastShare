4d3cb1c8a15802df272efb8a6dd0ae65a75ed532
Mauro Mascarenhas <mauro.mascarenhas@nintersoft.com>
2019-12-02 01:12:25 -0300

    Front-end session status and database connection
    Added:
    
    - New servlet to allow front-end scripts to get information about user session.
    - Database connection handler.
    
    Update:
    
    - Home page.
    - Home page scripts.

--- FastShareCMS/src/main/java/br/edu/ufabc/fastsharecms/dao/DatabaseConn.java
+++ FastShareCMS/src/main/java/br/edu/ufabc/fastsharecms/dao/DatabaseConn.java
@@ -1,17 +1,49 @@
 package br.edu.ufabc.fastsharecms.dao;
 
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Mauro
  */
 public class DatabaseConn {
     
+    private Integer port;
+    private String hostname;
+    private String database;
+    private String username;
+    private String password;
+    private Connection m_connection;
+    
     private static DatabaseConn db = null;
     
-    private DatabaseConn(){}
+    private DatabaseConn(){
+        hostname = "localhost";
+        port = 3306;
+        database = "fastshare";
+        username = "root";
+        password = "test";
+        
+        try {
+            DriverManager.registerDriver(new Driver());
+            String url = String.format("jdbc:mysql://%s:%d:%s", hostname, port, database);
+            m_connection = DriverManager.getConnection(url, username, password);
+            System.out.println("DB Connection stablished successfully");
+        } catch (SQLException e) {
+            System.out.println("Could not connect");
+            System.out.println("Message : " + e.getMessage());
+        }
+    }
+    
     public static DatabaseConn getInstance(){
         if (db == null) db = new DatabaseConn();
         return db;
     }
     
+    public Connection connection(){
+        return this.m_connection;
+    }
 }
--- FastShareCMS/src/main/webapp/scripts/homefeeder.js
+++ FastShareCMS/src/main/webapp/scripts/homefeeder.js
@@ -1,9 +1,19 @@
+var id;
 var begin;
 var btLoadMode;
 var spnLoading;
 var postContainer;
 
+var test = [{
+    id : 500,
+    author : "anonymous",
+    title : "A Title",
+    description : "I do not know what could I have written here before! (Still don't know).",
+    image_url : "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
+}];
+
 function initializeEnv(){
+    id = -1;
     begin = true;
     btLoadMore = document.querySelector("#load_more");
     spnLoading = document.querySelector("#loading_spinner");
@@ -20,7 +30,8 @@ function getData(){
     req.onreadystatechange = function(){
         if (req.readyState === 4){
             if (req.status === 200){
-                populate(req.responseText);
+                populate(JSON.parse(req.responseText));
+                populate(test);
                 spnLoading.style = "visibility : hidden";
                 btLoadMore.style = "visibility : visible";
             }
@@ -32,19 +43,45 @@ function getData(){
     req.send();
 }
 
-function populate(elements){
-    properties = JSON.parse(elements);
-    
-    for (let k = 0; k < properties.length; ++k){
-        let text = document.creatElement("p");
+function populate(properties){
+    for (let i = 0; i < properties.length; ++i){
+        let text = document.createElement("p");
         let edit = document.createElement("a");
         let card = document.createElement("div");
+        let image = document.createElement("img");
         let author = document.createElement("p");
         let header = document.createElement("h4");
         let content = document.createElement("div");
 
-        card.class = "card mb-3";
-        //TODO :everything
+        card.classList.add("card");
+        card.classList.add("mb-3");
+        
+        image.class = "card-img-top";
+        image.src = properties[i]["image_url"];
+        image.alt = "Featured image";
+        
+        content.classList.add("card-body");
+        
+        header.classList.add("card-title");
+        header.textContent = properties[i]["title"];
+        
+        text.classList.add("card-text");
+        text.textContent = properties[i]["description"];
+        
+        author.classList.add("text-muted");
+        author.textContent = properties[i]["author"] + " ";
+        
+        edit.href = "/editor?action=\"edit\"&id=" + properties[i]["id"];
+        edit.textContent = "( EDIT )";
+        
+        card.appendChild(image);
+        card.appendChild(content);
+        content.appendChild(header);
+        content.appendChild(text);
+        content.appendChild(author);
+        author.appendChild(edit);
+        
+        document.querySelector("#posts").appendChild(card);
     }
 }
 
